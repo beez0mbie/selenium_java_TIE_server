6d25a651f15e4b48e514d3eb2ba61d391ac11815
z0mbie-bee <vzhik3268@gmail.com>
2017-05-15 21:12:30 +0300

    step07 create new Users before change UserEditPage

--- src/test/java/ru/st/selenium/MakeNewUsers.java
+++ src/test/java/ru/st/selenium/MakeNewUsers.java
@@ -6,23 +6,55 @@ import org.testng.annotations.Test;
 import ru.st.selenium.model.User;
 import ru.st.selenium.pages.TestBase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.testng.Assert.assertTrue;
 
 /**
  * Created by a.shmelkov on 05.05.2017.
  */
 public class MakeNewUsers extends TestBase {
+
     private User admin = new User()
             .setLogin("admin")
             .setPassword("admin");
     private User oti = new User()
-            .setLogin("oti09")
-            .setName("Winner")
-            .setPassword("otb")
+            .setLogin("oti02")
+            .setName("Иванов Иван Иванович")
+            .setPassword("oti")
             .setRole("Оператор ОТИ")
-            .setPhone("123")
+            .setPhone("8-800-555-35-35")
             .setMetroStation("Каширская")
             .setMetroLine("Сокольническая линия");
+
+    private User otb = new User()
+            .setLogin("otb02")
+            .setName("Mr. Sexual Provider")
+            .setPassword("otb")
+            .setRole("Оператор ОТБ")
+            .setPhone("+79090006699")
+            .setMetroStation("Каширская")
+            .setMetroLine("Сокольническая линия");
+
+    private User dispetcher = new User()
+            .setLogin("disp02")
+            .setName("ЛЮДКА ХЛЕБО-БОРОДИНА")
+            .setPassword("disp")
+            .setRole("Диспетчер")
+            .setPhone("0006699")
+            .setMetroStation("Планерная")
+            .setMetroLine("Таганско-Краснопресненская линия");
+
+    private User od = new User()
+            .setLogin("od02")
+            .setName("H4ck the %хакерман;%?$\\//<>`(){[0]}")
+            .setPassword("disp")
+            .setRole("Оперативный дежурный")
+            .setPhone("0000000000")
+            .setMetroStation("Планерная")
+            .setMetroLine("Таганско-Краснопресненская линия");
+
 
     @BeforeMethod
     public void mayBeLogoutBefore() {
@@ -41,9 +73,17 @@ public class MakeNewUsers extends TestBase {
 
     @Test(description = "ID = <T01> Создать пользователя")
     public void test01() throws Exception {
-        app.getUserHelper().loginAs(admin);
-        app.getUserHelper().createNewUser(oti);
-        assertTrue(app.getUserHelper().isUserInUsersTable(oti));
+        ArrayList<User> users = new ArrayList<User>();
+        users.add(oti);
+        users.add(otb);
+        users.add(dispetcher);
+        users.add(od);
+        for (User user : users) {
+            app.getUserHelper().loginAs(admin);
+            app.getUserHelper().createNewUser(user);
+            assertTrue(app.getUserHelper().isUserInUsersTable(user));
+            app.getUserHelper().logout();
+        }
 
     }
 }
